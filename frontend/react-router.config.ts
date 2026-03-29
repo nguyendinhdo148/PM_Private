fe5747f9ceabb7e04f96e696df2f06e7b72dd057
nguyendinhdo148 <nguyendinhdo2k7@gmail.com>
2026-03-29 10:27:41 +0700

    1

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Bật chế độ SPA (Single Page Application) cho Static Site
   ssr: false,
+  prerender: ["/"], // Ép nó phải tạo file index.html cho trang chủ
 } satisfies Config;
